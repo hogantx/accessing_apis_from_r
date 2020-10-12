711aa33ddc7b78d2ad9b786f8515e048a90ba79b
Hogan <bhogan@amd.com>
2020-10-12 18:33:35 -0500

    Create accessing_apis_from_r.R

---
+++ accessing_apis_from_r.R
@@ -0,0 +1,54 @@
+
+
+install.packages("httr")
+
+
+library(httr)
+library(lubridate)
+library(jsonlite)
+
+remove.packages("plotly")
+remove.packages("sparklyr")
+
+library(httr)
+library(jsonlite)
+
+
+options(stringsAsFactors = FALSE)
+
+
+### read API
+url  <- "http://api.epdb.eu"
+path <- "eurlex/directory_code"
+raw.result <- GET(url = url, path = path)
+
+### inspect data, get status code
+names(raw.result)
+raw.result$status_code
+
+### convert from json
+this.raw.content <- rawToChar(raw.result$content)
+this.content <- fromJSON(this.raw.content)
+
+class(this.content)
+this.content[[1]]
+this.content[[2]]
+
+
+
+
+
+url <- "https://data.austintexas.gov/resource/2hik-84fu.json"
+raw.result <- GET(url = url)
+
+
+
+library(RSocrata)
+
+
+
+tmp <- read.socrata(url, stringsAsFactors = FALSE)
+str(tmp)
+
+table(tmp$zip_code)
+
