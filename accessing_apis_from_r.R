43d0cb3601d7f4d88abad5aa39b19c2c7b50d155
Hogan <bhogan@amd.com>
2020-10-13 18:53:35 -0500

    Update accessing_apis_from_r.R

--- accessing_apis_from_r.R
+++ accessing_apis_from_r.R
@@ -36,18 +36,16 @@ this.content[[2]]
 
 
 
-
-
-url <- "https://data.austintexas.gov/resource/2hik-84fu.json"
-raw.result <- GET(url = url)
-
-
-
 library(RSocrata)
 
+a <- "https://data.austintexas.gov/resource/spbg-9v94.json" #2015
+b <- "https://data.austintexas.gov/resource/8iue-zpf6.json" #2016
+c <- "https://data.austintexas.gov/resource/3t4q-mqs5.json" #2017
+d <- "https://data.austintexas.gov/resource/pgvh-cpyq.json" #2018
 
 
-tmp <- read.socrata(url, stringsAsFactors = FALSE)
+crime_reports <- "https://data.austintexas.gov/resource/fdj4-gpfu.json"
+tmp <- read.socrata(crime_reports, stringsAsFactors = FALSE)
 str(tmp)
 
 tmp$rep_time <- as.numeric(tmp$rep_time)
@@ -60,6 +58,14 @@ sort(table(tmp$crime_type))
 
 
 str(tmp)
+
+library(lubridate)
+tmp$year <- year(tmp$occ_date)
+table(tmp$year)
+
+
+setwd("C:/Users/bhogan/Documents/R/austin open data")
+saveRDS(tmp, file = "austin_police_crime_2003_2020") 
 
 library(dplyr)
 tmp <- tmp %>% filter(zip_code == "78750")
